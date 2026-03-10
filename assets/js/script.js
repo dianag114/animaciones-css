e73928eb4dea6224115251dca594252bbb8a7b06
dianag114 <gonzalezriveradiana377@gmail.com>
2026-03-10 12:49:20 -0600

    Versión 3: Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,10 @@
-// Lógica para la Magic Nav Bar (Activación al pasar el mouse)
+// Activa la burbuja de la barra de navegación al pasar el cursor
 const list = document.querySelectorAll('.list');
 
 function activeLink() {
-    // Removemos la clase activa de todos
     list.forEach((item) => item.classList.remove('active'));
-    // Se la agregamos al elemento sobre el que está el cursor
     this.classList.add('active');
 }
 
-// Cambiamos 'click' por 'mouseenter'
-list.forEach((item) => item.addEventListener('mouseenter', activeLink));
-
-// Nota: El código JS del menú radial se eliminó porque 
-// ahora lo controlaremos de forma más nativa y fluida con CSS.
+// Usamos mouseenter para que no sea necesario hacer clic
+list.forEach((item) => item.addEventListener('mouseenter', activeLink));    
